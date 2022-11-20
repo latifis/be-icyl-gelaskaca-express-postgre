f2a528f513463051c3c34ff882451a178cb9d9d8
Latief Irfansyah <latifirfansyah@gmail.com>
2022-11-21 06:39:13 +0700

    init config controller middleware models routes

---
+++ app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "latief-secret-key"
+  };
--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const cors = require("cors");
 
 const app = express();
 
+const db = require("./app/models");
+const Role = db.role;
+
+db.sequelize.sync({force: true}).then(() => {
+  console.log('Drop and Resync Db');
+  initial();
+});
+
 var corsOptions = {
   origin: "http://localhost:8081"
 };
@@ -18,7 +26,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to application." });
 });
 
 // set port, listen for requests
@@ -26,3 +34,20 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user"
+  });
+ 
+  Role.create({
+    id: 2,
+    name: "pic"
+  });
+ 
+  Role.create({
+    id: 3,
+    name: "admin"
+  });
+}
