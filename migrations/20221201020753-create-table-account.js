484fe98dafe27beae8a83c541f298a30a96fdd93
Latief Irfansyah <latifirfansyah@gmail.com>
2022-12-01 10:21:50 +0700

    add account migration

---
+++ migrations/20221201020753-create-table-account.js
@@ -0,0 +1,26 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("accounts", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("accounts");
+  },
+};
