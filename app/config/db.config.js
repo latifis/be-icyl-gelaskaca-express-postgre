2e6defa6aefb8f7e878cbdc1b1cd8553f80db74b
Latief Irfansyah <latifirfansyah@gmail.com>
2022-12-02 10:58:05 +0700

    add temp config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -11,3 +11,27 @@ module.exports = {
       idle: 10000
     }
   };
+
+  // {
+  //   "development": {
+  //     "username": "root",
+  //     "password": "",
+  //     "database": "gelaskaca",
+  //     "host": "127.0.0.1",
+  //     "dialect": "postgres"
+  //   },
+  //   "test": {
+  //     "username": "root",
+  //     "password": "",
+  //     "database": "gelaskaca",
+  //     "host": "127.0.0.1",
+  //     "dialect": "postgres"
+  //   },
+  //   "production": {
+  //     "username": "root",
+  //     "password": "",
+  //     "database": "gelaskaca",
+  //     "host": "127.0.0.1",
+  //     "dialect": "postgres"
+  //   }
+  // }
