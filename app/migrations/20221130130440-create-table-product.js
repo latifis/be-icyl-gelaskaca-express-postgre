18d8a3ae6c46cd138ca204567adffc62b30ce3ff
Latief Irfansyah <latifirfansyah@gmail.com>
2022-12-08 10:30:02 +0700

    fix products migration

--- app/migrations/20221130130440-create-table-product.js
+++ app/migrations/20221130130440-create-table-product.js
@@ -29,16 +29,6 @@ module.exports = {
         type: Sequelize.DATE,
         allowNull: false,
       },
-      cartId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: "carts",
-          key: "id",
-        },
-        onUpdate: "CASCADE",
-        onDelete: "CASCADE",
-      },
       categoryId: {
         type: Sequelize.INTEGER,
         allowNull: false,
